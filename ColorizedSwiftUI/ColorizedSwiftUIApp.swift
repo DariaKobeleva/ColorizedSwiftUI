0c3b7da16e304c3ad8c0a89f07f2a8a9e6f23dfc
KDaria <dkdva@mail.ru>
2024-04-19 15:39:58 +0200

    Initial Commit

---
+++ ColorizedSwiftUI/ColorizedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIApp.swift
+//  ColorizedSwiftUI
+//
+//  Created by Дарья Кобелева on 19.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
